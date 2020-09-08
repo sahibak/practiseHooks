b5700cb2c450963c2c830240b487791bb8e5b557
Sahiba Khanna <skhanna@konradgroup.com>
2020-09-08 08:45:56 -0400

    cleanup of update component

--- datadisplay/src/components/update.component.js
+++ datadisplay/src/components/update.component.js
@@ -1,31 +1,37 @@
-import React, { useContext, useRef, useEffect } from "react";
+import React, { useContext } from "react";
 import { DataContext } from "../context/context";
 import _, { initial } from "lodash";
 
 export default function Update(props) {
   const dataSetup = useContext(DataContext);
-  console.log("printing props", props.breweryToUpdate);
+
+  let updateType = (event, newType) => {
+    /**
+     * To update brewery type and then reset the update field to blank
+     * @param event onSubmit event
+     * @param {string} newType updated type of the brewery
+     */
+    event.preventDefault();
+    let intialData = _.cloneDeep(dataSetup.data);
+    console.log("iiiii", intialData.breweryData[0]);
+    for (let i = 0; i < intialData.breweryData.length; i++) {
+      if (intialData.breweryData[i].name === props.breweryToUpdate) {
+        intialData.breweryData[i].brewery_type = newType;
+        break;
+      }
+    }
+    console.log("$$$$", intialData.breweryData[0]);
+    console.log("####", dataSetup.data.breweryData[0]);
+    dataSetup.setData(intialData);
+    /*Clear the type entered */
+    newType = "";
+  };
+
   return (
     <>
-      <form
-        onSubmit={(event) => {
-          let newType = "macro";
-          event.preventDefault();
-          let intialData = _.cloneDeep(dataSetup.data);
-          console.log("iiiii", intialData.breweryData[0]);
-          for (let i = 0; i < intialData.breweryData.length; i++) {
-            if (intialData.breweryData[i].name === props.breweryToUpdate) {
-              intialData.breweryData[i].brewery_type = newType;
-              break;
-            }
-          }
-          console.log("$$$$", intialData.breweryData[0]);
-          console.log("####", dataSetup.data.breweryData[0]);
-          dataSetup.setData(intialData);
-        }}
-      >
+      <form onSubmit={(event) => updateType(event, event.target.newType.value)}>
         <label>New Brewery Type</label>
-        <input placeholder="Enter new type"></input>
+        <input name="newType" placeholder="Enter new type"></input>
         <button type="submit">Enter</button>
       </form>
     </>
