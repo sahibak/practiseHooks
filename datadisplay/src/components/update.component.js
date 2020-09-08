eba79bba6c2e09a6c60328b3ce5250ef414eebb9
Sahiba Khanna <skhanna@konradgroup.com>
2020-09-08 08:35:26 -0400

    added lodash for deep copy

--- datadisplay/src/components/update.component.js
+++ datadisplay/src/components/update.component.js
@@ -1,5 +1,6 @@
 import React, { useContext, useRef, useEffect } from "react";
 import { DataContext } from "../context/context";
+import _, { initial } from "lodash";
 
 export default function Update(props) {
   const dataSetup = useContext(DataContext);
@@ -10,28 +11,17 @@ export default function Update(props) {
         onSubmit={(event) => {
           let newType = "macro";
           event.preventDefault();
-          let intialData = [...dataSetup.data.breweryData];
-          console.log("iiiii", intialData[0]);
-          intialData[0].brewery_type = "new";
-          //   testing
-          //   for (let i = 0; i < intialData.length; i++) {
-          //     if (intialData[i].name === props.breweryToUpdate) {
-          //       console.log(
-          //         "enter if",
-          //         intialData[i].name,
-          //         intialData[i].brewery_type
-          //       );
-          //       intialData[i].brewery_type = newType;
-          //       break;
-          //     }
-          //   }
-          console.log("####", dataSetup.data);
-          //   dataSetup.setData({
-          //     breweryData: breweryData,
-          //     page: 1,
-          //     nameSearch: "",
-          //     typeSearch: "",
-          //   });
+          let intialData = _.cloneDeep(dataSetup.data);
+          console.log("iiiii", intialData.breweryData[0]);
+          for (let i = 0; i < intialData.breweryData.length; i++) {
+            if (intialData.breweryData[i].name === props.breweryToUpdate) {
+              intialData.breweryData[i].brewery_type = newType;
+              break;
+            }
+          }
+          console.log("$$$$", intialData.breweryData[0]);
+          console.log("####", dataSetup.data.breweryData[0]);
+          dataSetup.setData(intialData);
         }}
       >
         <label>New Brewery Type</label>
